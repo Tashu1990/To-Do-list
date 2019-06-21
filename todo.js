49697ff48c4af87fda38a4600de8df7d02dc57a7
Tashu1990 <tashusingh0612@gmail.com>
2019-06-21 09:41:40 +0530

    delete

---
+++ todo.js
@@ -0,0 +1,21 @@
+$("ul").on("click","li",function() {
+ $(this).toggleClass("comp");
+});
+
+$("ul").on("click","span",function(event){
+  $(this).parent().fadeOut(500,function () {
+  	$(this).remove();
+      });
+     event.stopPropogation();
+});
+ 
+ $("input[type='text'").keypress(function(event){
+ 	if(event.which ===13) {
+ 		var texttodo= $(this).val();
+ 		 $(this).val("");
+          $("ul").append("<li> <input type='checkbox'>" +"  "+texttodo+"<span><i class='glyphicon glyphicon-trash'></i><span></li>");
+ 	}
+ });
+
+
+
